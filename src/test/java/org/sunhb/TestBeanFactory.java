4be7f9d7f05cefa3c4a604ae0607314243939a63
shbone <598924626@qq.com>
2023-12-25 22:22:54 +0800

    update

---
+++ src/test/java/org/sunhb/TestBeanFactory.java
@@ -0,0 +1,34 @@
+package org.sunhb;
+
+import junit.framework.Assert;
+import org.junit.jupiter.api.Test;
+import org.sunhb.beans.BeanException;
+import org.sunhb.beans.factory.BeanFactory;
+import org.sunhb.beans.factory.config.BeanDefinition;
+import org.sunhb.beans.factory.support.DefaultListableBeanFactory;
+
+/**
+ * @author: SunHB
+ * @createTime: 2023/12/23 下午9:24
+ * @description:
+ */
+public class TestBeanFactory {
+    //@Test
+    //public void testBeanFactory(){
+    //    BeanFactory beanFactory = new BeanFactory();
+    //    beanFactory.registerBean("helloWorld",new HelloWorldBean());
+    //    HelloWorldBean  helloWorld = (HelloWorldBean)beanFactory.getBean("helloWorld");
+    //    Assert.assertNotNull(helloWorld);
+    //    Assert.assertEquals(helloWorld.hello(),"HelloWorldBean.hello");
+    //
+    //}
+    @Test
+    public void beanDefinitionAndBeanRegisterTest() throws BeanException {
+        DefaultListableBeanFactory defaultListableBeanFactory = new DefaultListableBeanFactory();
+        BeanDefinition helloWorldBeanDefinition = new BeanDefinition(HelloWorldBean.class);
+        defaultListableBeanFactory.registerBeanDefinition("helloWorldBean",helloWorldBeanDefinition);
+        HelloWorldBean helloWorldBean = (HelloWorldBean)defaultListableBeanFactory.getBean("helloWorldBean");
+        Assert.assertEquals(helloWorldBean.hello(),"HelloWorldBean.hello");
+    }
+
+}
