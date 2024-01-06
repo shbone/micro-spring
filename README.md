1dce62f10eb149076c2a18b12849cb9118f7a98b
shbone <598924626@qq.com>
2024-01-06 17:54:12 +0800

    docs:update picture

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-- bean构建流程图
+## 说明
+
+### bean构建流程图
+
 ![bean构建方法](./assets/pics/bean构建.png)
+
+### 层次结构图
+
+![](./assets/pics/层次结构图.png)
---
+++ src/main/java/org/sunhb/beans/factory/support/ApplicationContext.java
@@ -0,0 +1,12 @@
+package org.sunhb.beans.factory.support;
+
+import org.sunhb.beans.factory.HierarchicalBeanFactory;
+import org.sunhb.beans.factory.ListableBeanFactory;
+
+/**
+ * @author: SunHB
+ * @createTime: 2024/01/06 下午5:47
+ * @description:
+ */
+public interface ApplicationContext extends ListableBeanFactory, HierarchicalBeanFactory {
+}
